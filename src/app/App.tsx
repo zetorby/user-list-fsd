f1c20bf213cc00c4685237588d9e2ef7c3c455ec
den4ik <zetorby24@gmail.com>
2023-07-29 13:48:50 +0300

    template

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Router } from '~/pages';
+
+
+function App() {
+  return (
+   <>
+     <Router />
+   </>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { LandingPage } from './landing-page';
+import { ROUTES } from '~/shared/utils';
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path={ROUTES.landing.path} element={<LandingPage />} />
+    </Routes>
+  )
+}
